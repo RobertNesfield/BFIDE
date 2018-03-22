c4d797ca785ea9ff7a96e7519430aa4759542f26
robert <robert@nesfield.co.uk>
2018-03-22 18:00:53 +0000

    Done

--- src/Interface.java
+++ src/Interface.java
@@ -1,4 +1,5 @@
-import javafx.application.Application;
+import javafx.application.*;
+import javafx.collections.FXCollections;
 import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
@@ -12,6 +13,10 @@ public class Interface extends Application{
 	private Group root;
 	private Label[]memory=new Label[256];
 	private Rectangle pointer;
+	private TextArea code,input,output;
+	private Button run,runF,stop,clear;
+	private ComboBox<Integer>speed;
+	private boolean fast=true;
 	
 	@Override
 	public void start(Stage primaryStage){
@@ -19,37 +24,175 @@ public class Interface extends Application{
 		
 		stage=primaryStage;
 		root=new Group();
-		scene=new Scene(root,640,750);
+		scene=new Scene(root,960,750);
 		stage.setScene(scene);
 		stage.setTitle("BF interpreter/debugger");
 		stage.sizeToScene();
 		stage.setResizable(false);
+		stage.setOnCloseRequest(e->System.exit(0));
 		
 		Rectangle rect=new Rectangle();
 		root.getChildren().add(rect);
 		rect.setX(0);
 		rect.setY(0);
-		rect.setWidth(640);
-		rect.setHeight(320);
+		rect.setWidth(960);
+		rect.setHeight(160);
 		
 		pointer=new Rectangle();
 		root.getChildren().add(pointer);
 		pointer.setX(0);
 		pointer.setY(0);
-		pointer.setWidth(40);
+		pointer.setWidth(30);
 		pointer.setHeight(20);
 		pointer.setFill(Color.DARKRED);
 		
 		for(int i=0;i<256;i++){
 			memory[i]=new Label("000");
 			root.getChildren().add(memory[i]);
-			memory[i].setLayoutX(40*(i%16)+8);
-			memory[i].setLayoutY(20*(i/16)+3);
+			memory[i].setLayoutX(30*(i%32)+4);
+			memory[i].setLayoutY(20*(i/32)+3);
 			memory[i].setFont(new Font("Courier New",12));
 			memory[i].setTextFill(Color.LIGHTGRAY);
 		}
 		
+		code=new TextArea();
+		root.getChildren().add(code);
+		code.setPrefWidth(470);
+		code.setPrefHeight(440);
+		code.setLayoutX(5);
+		code.setLayoutY(205);
+		code.setWrapText(true);
+		code.setFont(new Font("Courier New",8));
+		
+		output=new TextArea();
+		root.getChildren().add(output);
+		output.setPrefWidth(470);
+		output.setPrefHeight(440);
+		output.setLayoutX(485);
+		output.setLayoutY(205);
+		output.setFont(new Font("Courier New",8));
+		output.setEditable(false);
+		
+		input=new TextArea();
+		root.getChildren().add(input);
+		input.setPrefWidth(950);
+		input.setPrefHeight(90);
+		input.setLayoutX(5);
+		input.setLayoutY(655);
+		input.setWrapText(true);
+		input.setFont(new Font("Courier New",8));
+		
+		runF=new Button("Run");
+		root.getChildren().add(runF);
+		runF.setPrefWidth(100);
+		runF.setPrefHeight(25);
+		runF.setLayoutX(5);
+		runF.setLayoutY(170);
+		runF.setOnAction(e->{
+			if(!Interpreter.running()){
+				fast=true;
+				
+				Interpreter.init();
+				Platform.runLater(()->{
+					for(int i=0;i<256;i++)memory[i].setText("000");
+					pointer.setX(0);
+					pointer.setY(0);
+				});
+				
+				new Thread(()->Interpreter.run(code.getText(),input.getText(),true)).start();
+			}
+		});
+		
+		speed=new ComboBox<>(FXCollections.observableArrayList(1,5,10,50,100,500,1000));
+		root.getChildren().add(speed);
+		speed.setPrefWidth(80);
+		speed.setPrefHeight(25);
+		speed.setLayoutX(310);
+		speed.setLayoutY(170);
+		speed.getSelectionModel().selectLast();
+		speed.getSelectionModel().selectPrevious();
+		speed.getSelectionModel().selectPrevious();
+		
+		Label ips=new Label("Instructions per second");
+		root.getChildren().add(ips);
+		ips.setLayoutX(393);
+		ips.setLayoutY(175);
+		
+		run=new Button("Debug at");
+		root.getChildren().add(run);
+		run.setPrefWidth(100);
+		run.setPrefHeight(25);
+		run.setLayoutX(205);
+		run.setLayoutY(170);
+		run.setOnAction(e->{
+			if(!Interpreter.running()){
+				fast=false;
+				
+				Interpreter.init();
+				Platform.runLater(()->{
+					for(int i=0;i<256;i++)memory[i].setText("000");
+					pointer.setX(0);
+					pointer.setY(0);
+				});
+				
+				new Thread(()->Interpreter.run(code.getText(),input.getText(),false)).start();
+			}
+		});
+		
+		stop=new Button("Terminate");
+		root.getChildren().add(stop);
+		stop.setPrefWidth(100);
+		stop.setPrefHeight(25);
+		stop.setLayoutX(650);
+		stop.setLayoutY(170);
+		stop.setOnAction(e->Interpreter.stop());
+		
+		clear=new Button("Reset");
+		root.getChildren().add(clear);
+		clear.setPrefWidth(100);
+		clear.setPrefHeight(25);
+		clear.setLayoutX(855);
+		clear.setLayoutY(170);
+		clear.setOnAction(e->{
+			Interpreter.stop();
+			Interpreter.init();
+			Platform.runLater(()->{
+				for(int i=0;i<256;i++)memory[i].setText("000");
+				pointer.setX(0);
+				pointer.setY(0);
+				code.setText("");
+				input.setText("");
+				output.setText("");
+			});
+		});
+		
 		stage.show();
+		
+		new Thread(()->{
+			while(true){
+				Platform.runLater(()->{
+					if(!fast){
+						final int[]mem=Interpreter.memory();
+						final String buffer=(mem[0]>99)?"":(mem[0]>9)?"0":"00";
+						if(mem[1]<256){
+							memory[mem[1]].setText(buffer+String.valueOf(mem[0]));
+							pointer.setX(30*(mem[1]%32));
+							pointer.setY(20*(mem[1]/32));
+						}else{
+							pointer.setX(0);
+							pointer.setY(-20);
+						}
+					}
+					if(!output.getText().replaceAll("\\p{C}","").equals(Interpreter.output().replaceAll("\\p{C}","")))output.setText(Interpreter.output());
+				});
+				if(!fast){
+					Interpreter.step();
+					try{Thread.sleep(1000/speed.getValue());}catch(Exception e){}
+				}else try{Thread.sleep(1);}catch(Exception e){}
+			}
+		}).start();
+		
+		new Thread(()->Interpreter.run("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.","",fast)).start();
 	}
 	
 	public static void main(String[]args){launch(args);}
--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,28 +1,26 @@
 public class Interpreter{
-	private static byte[]bytes=new byte[256];
-	private static byte pointer;
+	private static byte[]bytes=new byte[30000];
+	private static int pointer;
 	private static String out,inp;
-	private static boolean stopped=true;
+	private static boolean stopped=true,pause=false;
 	
 	public static void init(){
-		for(int i=0;i<256;i++)bytes[i]=0;
+		for(int i=0;i<30000;i++)bytes[i]=0;
 		pointer=0;
+		out="";
 	}
 	
-	public static void run(String code,String input){
-		if(stopped&&brace(code)){
-			for(int i=0;i<256;i++)bytes[i]=0;
-			pointer=0;
-			out="";
+	public static void run(String code,String input,boolean fast){
+		if(brace(code)){
 			inp=input;
-			
 			stopped=false;
-			exec(code);
+			pause=false;
+			if(fast)execF(code);else exec(code);
 			stopped=true;
-		}
+		}else out="ERROR: Unmatched Braces";
 	}
 	
-	private static void exec(String code){
+	private static void execF(String code){
 		for(int i=0;i<code.length();i++){
 			if(stopped)break;
 			switch(code.charAt(i)){
@@ -34,12 +32,14 @@ public class Interpreter{
 					break;
 				case'>':
 					pointer++;
+					if(pointer==30000)pointer=0;
 					break;
 				case'<':
 					pointer--;
+					if(pointer==-1)pointer=29999;
 					break;
 				case'.':
-					out+=(char)bytes[pointer];
+					out+=(char)(bytes[pointer]&0xFF);
 					break;
 				case',':
 					if(inp.length()==0)bytes[pointer]=0;
@@ -62,9 +62,62 @@ public class Interpreter{
 						}
 					}
 					
-					while(!stopped&&bytes[pointer]!=0)exec(code.substring(i+1,j));
+					while(!stopped&&bytes[pointer]!=0)execF(code.substring(i+1,j));
 					i=j;
 			}
+		}
+	}
+	
+	private static void exec(String code){
+		for(int i=0;i<code.length();i++){
+			while(pause&&!stopped)System.out.print("");
+			if(stopped)break;
+			switch(code.charAt(i)){
+				case'+':
+					bytes[pointer]++;
+					break;
+				case'-':
+					bytes[pointer]--;
+					break;
+				case'>':
+					pointer++;
+					if(pointer==30000)pointer=0;
+					break;
+				case'<':
+					pointer--;
+					if(pointer==-1)pointer=29999;
+					break;
+				case'.':
+					out+=(char)(bytes[pointer]&0xFF);
+					break;
+				case',':
+					if(inp.length()==0)bytes[pointer]=0;
+					else{
+						bytes[pointer]=(byte)inp.charAt(0);
+						inp=inp.substring(1);
+					}
+					break;
+				case'[':
+					int toFind=1,j=i;
+					
+					while(toFind!=0){
+						j++;
+						switch(code.charAt(j)){
+							case'[':
+								toFind++;
+								break;
+							case']':
+								toFind--;
+						}
+					}
+					
+					while(!stopped&&bytes[pointer]!=0){
+						pause=true;
+						exec(code.substring(i+1,j));
+					}
+					i=j;
+			}
+			pause=true;
 		}
 	}
 	
@@ -85,9 +138,9 @@ public class Interpreter{
 		return toFind==0;
 	}
 	
+	public static void step(){pause=false;}
 	public static void stop(){stopped=true;}
 	public static boolean running(){return !stopped;}
 	public static String output(){return out;}
-	public static byte[] memory(){return bytes.clone();}
-	public static int position(){return pointer;}
+	public static int[]memory(){return new int[]{bytes[pointer]&0xFF,pointer};}
 }
